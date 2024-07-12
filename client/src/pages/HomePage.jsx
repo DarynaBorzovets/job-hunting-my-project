c7ed73693369af36737cddfbc582d9fb3fdbc31c
DarynaBorzovets <biz.daryna@gmail.com>
2024-07-11 22:40:19 -0400

    Changed the description

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -5,12 +5,13 @@ const Home = () => {
         <div className="max-w-screen-xl p-8 bg-white rounded-lg shadow-lg">
           <h1 className="text-3xl font-bold mb-4">Job/Employees hunting Problem</h1>
           <p className="text-lg mb-4">
-          There is a significant number of immigrants who are seeking employment but face challenges such as language barriers, lack of formal education, and limited research skills. At the same time, many employers, also immigrants themselves and new business owners, may also face language barriers and seek employees who understand their specific cultural background, as their businesses cater primarily to immigrants from a particular country.
+          Many immigrants face employment challenges due to language barriers, lack of formal education, and limited research skills. Employers, often immigrants themselves, may also struggle with language barriers and seek employees who understand their specific cultural background.
+
           </p><br/>
           <h1 className="text-3xl font-bold mb-4">Solution</h1>
   
           <p className="text-lg mb-4">
-          To address this issue, I have created an application that enables applicants to filter job opportunities by language. Additionally, businesses can effortlessly publish job posts in their native language.
+          To address this issue, I have created an application that enables people  to filter job opportunities by language. Additionally, businesses can effortlessly publish job posts in their native language.
           </p>
         </div>
       </div>
